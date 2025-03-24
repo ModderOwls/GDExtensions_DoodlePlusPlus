48910b2ea3de5013130729a453af09dab53b040e
ModderOwls <lfjvanderheiden@live.nl>
2025-03-24 20:14:25 +0100

    Feat(Player): Player finished

--- src/player_physics.cpp
+++ src/player_physics.cpp
@@ -15,22 +15,30 @@ uint32_t rayMask = 0b01;
 
 void player_physics::_bind_methods() 
 {	
-	//Export variables to inspector.
+	//Export variables to inspector.	
+	ClassDB::bind_method(D_METHOD("get_acceleration"), &player_physics::get_acceleration);
+	ClassDB::bind_method(D_METHOD("set_acceleration", "new_acceleration"), &player_physics::set_acceleration);
+	
+	ClassDB::bind_method(D_METHOD("get_topSpeed"), &player_physics::get_topSpeed);
+	ClassDB::bind_method(D_METHOD("set_topSpeed", "new_topSpeed"), &player_physics::set_topSpeed);
+
 	ClassDB::bind_method(D_METHOD("get_gravity"), &player_physics::get_gravity);
 	ClassDB::bind_method(D_METHOD("set_gravity", "new_gravity"), &player_physics::set_gravity);
 
-	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "gravity"), "set_gravity", "get_gravity");
+	ClassDB::bind_method(D_METHOD("get_jumpHeight"), &player_physics::get_jumpHeight);
+	ClassDB::bind_method(D_METHOD("set_jumpHeight", "new_jumpHeight"), &player_physics::set_jumpHeight);
 
-	
-	ClassDB::bind_method(D_METHOD("get_moveSpeed"), &player_physics::get_moveSpeed);
-	ClassDB::bind_method(D_METHOD("set_moveSpeed", "new_moveSpeed"), &player_physics::set_moveSpeed);
+	ClassDB::bind_method(D_METHOD("get_drag"), &player_physics::get_drag);
+	ClassDB::bind_method(D_METHOD("set_drag", "new_drag"), &player_physics::set_drag);
 
-	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "moveSpeed"), "set_moveSpeed", "get_moveSpeed");
-
-	
 	ClassDB::bind_method(D_METHOD("get_height"), &player_physics::get_height);
 	ClassDB::bind_method(D_METHOD("set_height", "new_height"), &player_physics::set_height);
 
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "acceleration"), "set_acceleration", "get_acceleration");
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "topSpeed"), "set_topSpeed", "get_topSpeed");
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "gravity"), "set_gravity", "get_gravity");
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "jumpHeight"), "set_jumpHeight", "get_jumpHeight");
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "drag"), "set_drag", "get_drag");
 	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "height"), "set_height", "get_height");
 }
 
@@ -38,7 +46,6 @@ player_physics::player_physics()
 {
 	deltaPhysF = 0;
 
-	grounded = false;
 	velocity = Vector2(0, 0);
 }
 
@@ -49,7 +56,7 @@ void player_physics::_ready()
 	Engine *engine = Engine::get_singleton();
 
 	//Set up raycast variables.
-	//Assuming it's like C#/.Net, setting this every frame/tick causes garbage.
+	//Assuming it's like Godot C#/.Net, setting this every frame/tick creates garbage.
 	spaceState = CanvasItem::get_world_2d()->get_direct_space_state();
 	query = PhysicsRayQueryParameters2D::create(Vector2(0, 0), Vector2(0, -1), rayMask);
 
@@ -66,81 +73,97 @@ void player_physics::_ready()
 	}
 }
 
-void player_physics::_process(double delta) 
-{
-	
-}
-
 void player_physics::_physics_process(double delta)
 {
 	deltaPhysF = (float)delta;
 
-	if (grounded) movement_grounded();
-	else movement_air();
+	movement_air();
 
 	//Update physics.
-	Node2D::set_global_position(Node2D::get_global_position() + velocity);
-}
-
-void player_physics::movement_grounded()
-{
-	Vector2 globalPosition = Node2D::get_global_position();
-
-	velocity.x = inputHorizontal * moveSpeed;
-
-	//Perform raycast.
-	Dictionary ray = push_raycast(globalPosition, globalPosition + Vector2(0, height / 2 + .1f));
-
-	//Detect for a floor.
-	if (!ray.is_empty())
-	{
-		//Make player stick to floor on ramps.
-		Node2D::set_global_position((Vector2)ray["position"] + Vector2(0, -1) * height / 2);
-	}
-	else
-	{
-		grounded = false;
-
-		print_line("ray lost floor.");
-	}
+	Node2D::set_position(Node2D::get_position() + velocity * deltaPhysF);
 }
 
 void player_physics::movement_air()
 {
-	Vector2 globalPosition = Node2D::get_global_position();
+	Vector2 position = Node2D::get_position();
+
+	velocity.x += inputHorizontal * acceleration * deltaPhysF;
+
+    //Apply drag when not holding a direction or standing still.
+    if (inputHorizontal == 0)
+    {
+        if (velocity.x != 0)
+        {
+            float dragDelta = drag * deltaPhysF;
+
+			//Ensure it doesn't push it below zero.
+            if (abs(velocity.x) <= dragDelta)
+            {
+                velocity.x = 0;
+            }
+            else
+            {
+                velocity.x -= SIGN(velocity.x) * dragDelta;
+            }
+        }
+    }
+
+	//Cap max speed of player.
+	if (abs(velocity.x) > topSpeed)
+	{
+		velocity.x = topSpeed * SIGN(velocity.x);
+	}
 
 	velocity.y += gravity * deltaPhysF;
 
 	//Perform raycast.
-	Dictionary ray = push_raycast(globalPosition, globalPosition + Vector2(0, height / 2 + .1f));
+	Dictionary ray = push_raycast(position, Vector2(0, height / 2 + .1f));
 
-	//Detect for a floor.
-	if (!ray.is_empty())
+	//Detect for a floor only when going down.
+	if (!ray.is_empty() && velocity.y >= 0)
 	{
-		grounded = true;
-
-		velocity.y = 0;
-
-		Node2D::set_global_position((Vector2)ray["position"] + Vector2(0, -1) * height / 2);
-
-		print_line("ray hit floor!");
+		on_floor_hit(ray);
 	}
+}
+
+void player_physics::on_floor_hit(Dictionary& ray)
+{
+	velocity.y = -jumpHeight;
+
+	Node2D::set_position((Vector2)ray["position"] + Vector2(0, -1) * height / 2);
 }
 
 void player_physics::_unhandled_input(const Ref<InputEvent> &input)
 {
 	if (input->is_echo() || !input->is_action_type()) return;
 
+	//Simple script for more responsive feeling controls for keyboards. Inspired by Snap Tap.
 	if (input->is_action("playerRight"))
 	{
-		inputHorizontal = input->get_action_strength("playerRight");
+		inputRight = input->get_action_strength("playerRight");
+
+		if (input->is_pressed())
+		{
+			inputHorizontal = inputRight;
+		}
+		else
+		{
+			inputHorizontal = -inputLeft;
+		}
 	}
 	else if (input->is_action("playerLeft"))
 	{
-		inputHorizontal = -input->get_action_strength("playerLeft");
+		inputLeft = input->get_action_strength("playerLeft");
+		
+		if (input->is_pressed())
+		{
+			inputHorizontal = -inputLeft;
+		}
+		else
+		{
+			inputHorizontal = inputRight;
+		}
 	}
-
-	//delete(&input);
 }
 
 Dictionary player_physics::push_raycast(const Vector2 from, const Vector2 to)
@@ -151,13 +174,26 @@ Dictionary player_physics::push_raycast(const Vector2 from, const Vector2 to)
 	return spaceState->intersect_ray(query); //do raycast thingamajig
 }
 
+
+//MoveSpeed s(g)etters
+void player_physics::set_acceleration(const float new_acceleration) { acceleration = new_acceleration; }
+float player_physics::get_acceleration() const { return acceleration; }
+
+//TopSpeed s(g)etters
+void player_physics::set_topSpeed(const float new_topSpeed) { topSpeed = new_topSpeed; }
+float player_physics::get_topSpeed() const { return topSpeed; }
+
 //Gravity s(g)etters
 void player_physics::set_gravity(const float new_gravity) { gravity = new_gravity; }
 float player_physics::get_gravity() const { return gravity; }
 
-//MoveSpeed s(g)etters
-void player_physics::set_moveSpeed(const float new_moveSpeed) { moveSpeed = new_moveSpeed; }
-float player_physics::get_moveSpeed() const { return moveSpeed; }
+//JumpHeight s(g)etters
+void player_physics::set_jumpHeight(const float new_jumpHeight) { jumpHeight = new_jumpHeight; }
+float player_physics::get_jumpHeight() const { return jumpHeight; }
+
+//Drag s(g)etters
+void player_physics::set_drag(const float new_drag) { drag = new_drag; }
+float player_physics::get_drag() const { return drag; }
 
 //Height s(g)etters
 void player_physics::set_height(const float new_height) { height = new_height; }
--- src/player_physics.h
+++ src/player_physics.h
@@ -12,10 +12,13 @@ namespace godot
 		GDCLASS(player_physics, Node2D)
 
 	private:
-		void movement_grounded();
 		void movement_air();
+		void on_floor_hit(Dictionary& ray);
 
 		Dictionary push_raycast(Vector2 from, Vector2 to);
+
+		float inputLeft = 0;
+		float inputRight = 0;
 
 		float deltaPhysF;
 
@@ -26,26 +29,36 @@ namespace godot
 		player_physics();
 		~player_physics();
 
-		float inputHorizontal;
+		float inputHorizontal = 0;
 
-		bool grounded;
 		Vector2 velocity;
 
-		float gravity = 1;
-		float moveSpeed = 2;
+		float acceleration = 640;
+		float topSpeed = 45;
+		float gravity = 72;
+		float jumpHeight = 64;
+		float drag = 300;
 
-		float height = 5;
+		float height = 8;
 
 		void _ready() override;
-		void _process(double delta) override;
 		void _physics_process(double delta) override;
 		void _unhandled_input(const Ref<InputEvent> &input) override;
+
+		void set_acceleration(const float new_acceleration);
+		float get_acceleration() const;
+
+		void set_topSpeed(const float new_topSpeed);
+		float get_topSpeed() const;
 
 		void set_gravity(const float new_gravity);
 		float get_gravity() const;
 
-		void set_moveSpeed(const float new_moveSpeed);
-		float get_moveSpeed() const;
+		void set_jumpHeight(const float new_jumpHeight);
+		float get_jumpHeight() const;
+
+		void set_drag(const float new_drag);
+		float get_drag() const;
 
 		void set_height(const float new_height);
 		float get_height() const;
